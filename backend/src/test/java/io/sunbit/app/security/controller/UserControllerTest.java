bc2534c2d6f0af8581d03d5bc514fa48247144a3
jdomdev <jdomingodev@duck.com>
2025-12-16 12:24:53 +0100

    test: mock jwt filter and activate profile for mvc controllers

--- backend/src/test/java/io/sunbit/app/security/controller/UserControllerTest.java
+++ backend/src/test/java/io/sunbit/app/security/controller/UserControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -31,6 +32,7 @@ import io.sunbit.app.security.service.UserServiceImpl;
 
 @SpringBootTest(properties = "app.jwt.secret=testsecretkeymustbelongenoughforhs512algorithmtoworkproperly1234567890")
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class UserControllerTest {
 
     @Autowired
